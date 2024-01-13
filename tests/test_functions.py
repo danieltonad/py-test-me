ef6f6cbb121df7a88e67ffbefbfdacc3c1bce3b4
danieltonad <soladan2000@gmail.com>
2024-01-13 06:34:09 -0900

    test sucks

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,6 +1,13 @@
 import pytest
-# import source.functions as functions
+from sources import functions as func
 
 
 def test_add():
-    pass
+    result = func.add(3, 2)
+    assert result == 5
+    
+
+def test_divide():
+    with pytest.raises(ZeroDivisionError):
+        func.divide(10, 0)
+    assert True
